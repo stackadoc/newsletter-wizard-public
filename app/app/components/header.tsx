2997fcb74f8a86b0eba63779f57bca46aa0299b7
Paul <paul.chaumeilv@gmail.com>
2025-04-07 23:06:39 +0200

    Homepage

--- app/app/components/header.tsx
+++ app/app/components/header.tsx
@@ -4,7 +4,7 @@ import {Link} from "react-router";
 
 const Header = () => {
     return (
-        <header className="border-b sticky top-0 bg-background z-10 w-[100vw]">
+        <header className="border-b sticky top-0 bg-background z-10">
             <nav className="flex items-center justify-between h-16 px-4 md:px-6 w-full">
                 <div className="flex items-center gap-4">
                     <Link to="/">
