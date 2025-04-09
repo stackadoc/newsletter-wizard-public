d6104f319aa810d1c84dffc14c81cb2c0ae2de25
Paul <paul.chaumeilv@gmail.com>
2025-04-09 18:24:03 +0200

    Update footer link style and route comments

--- app/app/components/footer.tsx
+++ app/app/components/footer.tsx
@@ -5,7 +5,7 @@ const Footer = () => {
         <footer className="border-t">
             <div className="flex items-center justify-center h-16 px-4 md:px-6 text-sm text-muted-foreground">
                 © {new Date().getFullYear()} Newsletter Wizard By&nbsp;
-                <Link to="https://www.stackadoc.com" target="_blank">Stackadoc</Link>
+                <Link to="https://www.stackadoc.com" target="_blank"><u>Stackadoc</u></Link>
                 . All rights reserved.
             </div>
         </footer>
--- app/app/routes.ts
+++ app/app/routes.ts
@@ -1,17 +1,5 @@
-// import {route, type RouteConfig} from "@react-router/dev/routes";
-// import {
-//     index,
-//     layout,
-// } from "@react-router/dev/routes";
-//
-// export default [
-//     layout("layouts/header-footer.tsx", [
-//         index("routes/home.tsx"),
-//     ]),
-//     route("action/set-theme", "routes/action.set-theme.ts"),
-// ] satisfies RouteConfig;
-
 import { type RouteConfig } from "@react-router/dev/routes";
 import { flatRoutes } from "@react-router/fs-routes";
 
+// Doc: https://reactrouter.com/how-to/file-route-conventions
 export default flatRoutes() satisfies RouteConfig;
